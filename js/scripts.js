06a0c4a445cc256c14f9a2e18ed953173183c366
aktechinquiries <aktechinquiries@gmail.com>
2025-05-28 20:30:42 -0400

    Update scripts.js
    If applied, this commit will have added a new variable called "pokemonList" and assigned it to a blank array. Then three keys will be added to each Pokemon object for the value of name (this will be a string), height (this will be a number), and types (this will be an array of strings).

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
-alert('Hello World');
-let favoriteFood = 'Collard Greens';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [{name: "Bulbasaur", height: "0.7", type: ["grass","poison"]},{name: "Charizard", height: "1.7", type: ["fire","flying"]},{name: "Butterfree", height: "1.1", type: ["bug","fyling"]}];
+
