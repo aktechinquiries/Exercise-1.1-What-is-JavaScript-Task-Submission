45c63ab5db974076da35ba3e66c80a97284fbc71
aktechinquiries <aktechinquiries@gmail.com>
2025-06-07 09:55:00 -0400

    Update script.js
    If applied, this commit will have changed the height values of the "pokemonList" from a string to a number, allowing future "If statements" to be processed easier.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 let pokemonList = [];
-pokemonList = [{name: "Bulbasaur", height: "0.7", type: ["grass","poison"]},{name: "Charizard", height: "1.7", type: ["fire","flying"]},{name: "Butterfree", height: "1.1", type: ["bug","fyling"]}];
+pokemonList = [{name: "Bulbasaur", height: 0.7, type: ["grass","poison"]},{name: "Charizard", height: 1.7, type: ["fire","flying"]},{name: "Butterfree", height: 1.1, type: ["bug","fyling"]}];
 
