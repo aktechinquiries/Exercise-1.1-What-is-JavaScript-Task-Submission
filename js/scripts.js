476d16ae41e5646fff1eee0e6f53b834684f478c
aktechinquiries <aktechinquiries@gmail.com>
2025-06-08 16:22:01 -0400

    Update scripts.js
    If applied, this commit will have added a for loop to iterate over each object (item) within the "pokemonList" array. The use of "document.write()" within the loop's code will write the Pokemon name on the websites DOM. A string was added to write the Pokemon's height next to its name inside the loop's code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-let pokemonList = [];
-pokemonList = [{name: "Bulbasaur", height: 0.7, type: ["grass","poison"]},{name: "Charizard", height: 1.7, type: ["fire","flying"]},{name: "Butterfree", height: 1.1, type: ["bug","fyling"]}];
+let pokemonList = [
+    {name: "Bulbasaur", height: 0.7, type: ["grass","poison"]},
+    {name: "Charizard", height: 1.7, type: ["fire","flying"]},
+    {name: "Butterfree", height: 1.1, type: ["bug","fyling"]}
+];
 
+for ( i = 0; i < pokemonList.length; i++) {
+    document.write("<p>" + pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ')' + "</p>");
+}
