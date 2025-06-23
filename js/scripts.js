416939b5ddb31906619c4201f4be0f86e0fba040
aktechinquiries <aktechinquiries@gmail.com>
2025-06-23 16:30:58 -0400

    Update scripts.js
    If applied, this commit will have changed the array by wrapping the "pokemonList" array in the IIFE to turn it into a self executing function so that it calls itself to avoid accidentally accessing the global state.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
 let pokemonRepository = (function () {
-
-})();
-let pokemonList = [
-    {
-        name: "Bulbasaur", 
-        height: 0.7, 
-        type: ["grass","poison"]
-    },
-    {
-        name: "Charizard", 
-        height: 1.7, 
-        type: ["fire","flying"]
-    },
-    {
-        name: "Butterfree", 
-        height: 1.1, 
-        type: ["bug","fyling"]
-    }
+    let pokemonList = [
+        {
+            name: "Bulbasaur", 
+            height: 0.7, 
+        },
+            name: "Charizard", 
+            height: 1.7, 
+        },
+        {
+            name: "Butterfree", 
+            height: 1.1, 
+            type: ["bug","fyling"]
+        }
     ];
+})();
+
 
 for ( i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height === 1.7) {
