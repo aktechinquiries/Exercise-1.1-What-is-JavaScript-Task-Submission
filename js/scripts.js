aeff74689029a4bb1b969d77e634d997985be6d8
aktechinquiries <aktechinquiries@gmail.com>
2025-07-19 14:18:45 -0400

    Update scripts.js
    If applied, this commit will have changed the "scripts.js" file by removing the code within the "pokemonRepository" forEach function that has the parameter "pokemon".

--- js/scripts.js
+++ js/scripts.js
@@ -27,17 +27,6 @@ let pokemonRepository = (function () {
 })();
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-    if (pokemon.height === 1.7) {
-        document.write(pokemon.name + ' ' + '(height: ' + pokemon.height + ')' + " - Wow, that's big!")
-    } else {
-        document.write("<p>" + pokemon.name + ' ' + '(height: ' + pokemon.height + ')' + "</p>");
-    }
+    
 });
 
-for ( i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height === 1.7) {
-        document.write(pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ')' + " - Wow, that's big!")
-    } else {
-        document.write("<p>" + pokemonList[i].name + ' ' + '(height: ' + pokemonList[i].height + ')' + "</p>");
-    }
-}
