967c6dd41e11c049eccd0e935fcc90a596180d89
aktechinquiries <aktechinquiries@gmail.com>
2025-10-12 11:04:46 -0400

    Update scripts.js
    If applied, this commit will have changed the "scripts.js" file by inserting the ESC-key code below the "hideModal" function so that the modal is removed when the user presses the ESC key.

--- js/scripts.js
+++ js/scripts.js
@@ -70,6 +70,13 @@ let pokemonRepository = (function () {
       }  
     }
     
+    window.addEventListener('keydown', (e) => {
+      let modalContainer = document.querySelector('#modal-container');
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();  
+      }
+    });
+
     document.querySelector('#show-modal').addEventListener('click', () => {
       showModal('Modal title', 'This is the modal content!');
     });
